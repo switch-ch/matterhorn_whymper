6affee988b72a1396f90ba0f72f1ea481a460b7e
Daniel Fritschi <daniel.fritschi@switch.ch>
2014-04-25 10:22:50 +0200

    tests added

--- spec/endpoint_api_spec.rb
+++ spec/endpoint_api_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe Matterhorn::EndpointApi do
 
+  it "version must be defined" do
+    expect(Matterhorn::EndpointApi::VERSION).not_to be nil
+  end
+
   it "#info" do
     expect(Matterhorn::EndpointApi.info).to include Matterhorn::EndpointApi::VERSION
   end
