6677268aec0b0d3dd48f468f1d5f06a31c91560c
Manfred Stienstra <manfred@fngtps.com>
2014-06-16 17:06:17 +0200

    Allow track to be added with URL instead of path.
    The current version of library assumes the first argument to addTrack is
    always a file path. The API also allows a URL to be passed.

---
+++ spec/unit/matterhorn/endpoint/ingest_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+require 'tempfile'
+
+describe Matterhorn::Endpoint::Ingest do
+  before do
+    @ingest = Matterhorn::Endpoint::Ingest.new
+    stub_request(:head, %r{}).to_return(status: 200, body: '', headers: { 'WWW-Authenticate' => 'Basic realm="Matterhorn"' })
+    stub_request(:get, %r{/ingest/createMediaPackage}).to_return(status: 200, body: file_fixture_contents('empty_media_package.xml'))
+  end
+
+  it "adds a local file to its media package with addTrack" do
+    stub_request(:post, %r{/ingest/addTrack}).to_return(status: 200, body: file_fixture_contents('media_package_with_one_track.xml'))
+    @ingest.createMediaPackage
+    tempfile = Tempfile.new('source')
+    tempfile.close
+    begin
+      response_body = @ingest.addTrack(tempfile.path, 'source/raw')
+      expect(response_body).to start_with('<?xml')
+    ensure
+      tempfile.unlink
+    end
+  end
+
+  it "adds a source URL to its media package with addTrack" do
+    stub_request(:post, %r{/ingest/addTrack}).to_return(status: 200, body: file_fixture_contents('media_package_with_one_track.xml'))
+    @ingest.createMediaPackage
+    response_body = @ingest.addTrack('http://example.com/path/to/file', 'source/raw')
+    expect(response_body).to start_with('<?xml')
+  end
+end
