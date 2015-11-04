27b76692ba0f5701f13c8d9c800e4df96b0714ba
Daniel Fritschi <daniel.fritschi@switch.ch>
2015-11-04 13:20:26 +0100

    check params of sign url method

--- lib/matterhorn/endpoint/security.rb
+++ lib/matterhorn/endpoint/security.rb
@@ -14,12 +14,22 @@ class Matterhorn::Endpoint::Security < Matterhorn::Endpoint
   # - valid_until: The date and time until the signed url is valid
   # - valid_source: The IP address from which the request is allowed 
   #
-  def sign(url, valid_until, valid_source)
+  def sign(url, valid_until = nil, valid_source = nil)
     signed_url = nil
     begin
+      params = {}
+      params['url'] = url.to_s
+      if !valid_until.nil? && (valid_until.kind_of?(DateTime) || valid_until.kind_of?(Time))
+        params['valid_until'] = valid_until.xmlschema
+      elsif !valid_until.nil? && valid_until.respond_to?(:to_s)
+        params['valid_until'] = valid_until.to_s
+      end
+      if !valid_source.nil? && valid_source.respond_to?(:to_s)
+        params['valid_source'] = ivalid_source.to_s
+      end
       split_response http_endpoint_client.post(
         "api/security/sign",
-        { 'url' => url.to_s, 'validate-until' => valid_until.to_s, 'valid-source' => valid_source.to_s }
+        params
       )
       signed_url = JSON.parse(response_body)['url']
     rescue => ex
